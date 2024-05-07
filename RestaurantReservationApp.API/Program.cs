83344e52b782bb905cb2e93e783fa10975e0ca5b
unknown <duje.miljevic@gmail.com>
2024-05-07 17:57:29 +0200

    created docker-compose with api and postgres db, created connection string and db

--- RestaurantReservationApp.API/Program.cs
+++ RestaurantReservationApp.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservationApp.Infrastructure.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDatabase")));
 
 var app = builder.Build();
 
---
+++ RestaurantReservationApp.Infrastructure/DatabaseContext/RestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservationApp.Infrastructure.DatabaseContext
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set;}
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
