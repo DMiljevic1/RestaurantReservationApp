e0b7ea9d2d4a227e2af013d17aa3cb25ca5cfdf1
unknown <duje.miljevic@gmail.com>
2024-05-11 15:48:19 +0200

    extracted services in folder from Program.cs

--- RestaurantReservationApp.API/Program.cs
+++ RestaurantReservationApp.API/Program.cs
@@ -1,29 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantReservationApp.API.Extensions;
-using RestaurantReservationApp.Domain.Repositories;
-using RestaurantReservationApp.Infrastructure.DatabaseContext;
-using RestaurantReservationApp.Infrastructure.Repositories;
-using MediatR;
-using Microsoft.Extensions.DependencyInjection;
-using RestaurantReservationApp.Application;
-using System.Reflection;
-using RestaurantReservationApp.Application.Common.User.Handlers;
+using RestaurantReservationApp.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.ConfigureServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDatabase")));
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AddUserHandler).Assembly));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
