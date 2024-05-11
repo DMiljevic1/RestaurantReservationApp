1120e657bea7a8360120f489a8b2187884162949
unknown <duje.miljevic@gmail.com>
2024-05-11 14:51:34 +0200

    implemeted add user using medator patern

---
+++ RestaurantReservationApp.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservationApp.Application.Common.User.Commands;
+using RestaurantReservationApp.Domain.Entities;
+
+namespace RestaurantReservationApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody]User user, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _mediator.Send(new AddUserCommand(user), cancellationToken);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                await Console.Out.WriteLineAsync(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ RestaurantReservationApp.Application/Common/User/Commands/AddUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantReservationApp.Domain;
+using RestaurantReservationApp.Domain.Entities;
+using System.Drawing;
+
+namespace RestaurantReservationApp.Application.Common.User.Commands
+{
+    public record AddUserCommand(Domain.Entities.User User) : IRequest;
+}
